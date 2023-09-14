bd19b95049440cb92644087e1f4b8022f4ad0de7
faltunik <niksanat8@gmail.com>
2023-09-14 15:31:25 +0530

    solved error

--- src/profiles/EditProfile.js
+++ src/profiles/EditProfile.js
@@ -59,7 +59,7 @@ export default function EditProfile({api, profile}) {
   //         console.log(response.data)
   //         console.log(typeof(response.data))
   //         console.log(response.data.image)
-  //         const newurl = `http://nitrr-anon.azurewebsites.net${response.data.image}`
+  //         const newurl = `https://nitrr-anon.azurewebsites.net${response.data.image}`
   //         response.data.image = newurl
   //         console.log(typeof(response.data.image))
           
